dd094659cc4d0dc53278b5f8099b3c0232f1408f
Travis Buckley <travispbuckley@lewisu.edu>
2016-08-29 17:08:59 -0500

    fixed error, added equal sign to hash

---
+++ ruby/hash_practice.rb
@@ -0,0 +1,11 @@
+int_job_app = {
+	name: "Travis",
+	address: "4711 Yender Aveneue",
+	email: "travispbuckley@lewisu.edu",
+	phone_num: "815-514-5400",
+	fav_blue_shade: "indigo",
+	wall_pap_pref: "Paisly, Chevrons",
+	def_ombre: "So last season"
+}
+
+p int_job_app
