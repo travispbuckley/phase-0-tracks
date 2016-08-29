fd533dedfcbabb9158f877c2cb69a53911ded119
Travis Buckley <travispbuckley@lewisu.edu>
2016-08-29 13:43:17 -0500

    answered all except 1

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -7,7 +7,7 @@
  "iNvEsTiGaTiOn".swapcase
 # => “InVeStIgAtIoN”
 
-# "zom".<???>
+"zom".insert(1, "o")
 # => “zoom”
 
  "enhance".center(15)
@@ -16,19 +16,19 @@
  "Stop! You’re under arrest!".upcase
 # => "STOP! YOU’RE UNDER ARREST!"
 
-# "the usual".<???>
+ "the usual".insert(9, " suspects")
 #=> "the usual suspects"
 
-# " suspects".<???>
+ "suspects".insert(-9, "The usual ")
 # => "the usual suspects"
 
  "The case of the disappearing last letter".chop
 # => "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".<???>
+ "The mystery of the missing first letter".reverse.chop.reverse
 # => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
+ "Elementary,    my   dear        Watson!".squeeze(" ")
 # => "Elementary, my dear Watson!"
 
 # "z".<???>
