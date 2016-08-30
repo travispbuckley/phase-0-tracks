74be0bba60d12020eb1c50b2823e5a10f06abb17
Travis Buckley <travispbuckley@lewisu.edu>
2016-08-29 19:03:11 -0500

    working on final part, allowing for user changes

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,13 +1,15 @@
 # Pseudocode
 # Intake Client details: Name, age, number of children, decor theme, 
 # married, years lived there, favorite color
-# 1st) Ask designer.user for this info
+# 1st) Ask designer/user for this info
 # 2nd) Convert user input to appropriate data type (.to_s .to_i)
 # 3rd) Print out hash after submission is complete
 # 4th) Give user chance to update key
 # 		-Designer can opt out of this
 # 		-if designer gives a key that needs updating, ask for new 
-# 			new value and update the key
+# 			new value and update the key.
+#			-Need to convert the user response (gets.chomp) into the symbol that 
+#			needs to be updated!!
 # 5th) Print hash and exit program
 
 def int_designer
@@ -43,8 +45,12 @@ def int_designer
 		residence: current_resid ,
 		color: user_color
 	}
-
 	p client_data
+	puts "Is this information correct? Please make any additional changes now.
+		  Choose from the follow list: name, age, children, decor, married, residence, or color.
+		  Type 'done' when complete"
+	data_update = gets.chomp.to_sym
+	
 
 end
 int_designer
