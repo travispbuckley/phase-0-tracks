c8231c57e3e07131433442311e0a70b4bac500e8
Travis Buckley <travispbuckley@lewisu.edu>
2016-08-31 15:41:56 -0500

    added home team hashes/arrys to human hash

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,16 @@
+hockey_rink = {
+	humans: {
+			home_team: {
+				offense: {
+						  forwards: 2 ,
+						  wingers: 6 
+						  },
+				defense: {
+						  aggressive: [
+						  			  'Buckley', 
+						  			  'Griffin'],
+						  passive: [
+						  		    'Howard', 
+						  		    'Gershman'] 
+						 }
+				},
