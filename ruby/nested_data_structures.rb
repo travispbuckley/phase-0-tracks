e4b11e5d7ad40a03597c0a3e4c268542b3339892
Travis Buckley <travispbuckley@lewisu.edu>
2016-08-31 16:17:51 -0500

    added statements showing ability to go through hashes

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -36,3 +36,13 @@ hockey_rink = {
 				warrior: ['Dynasty', 'Covert', 'QRL4']   }
 	}
 }
+puts "Who are the aggressive defensemen of the home team?"
+puts hockey_rink[:humans][:home_team][:defense][:aggressive]
+puts "Oh no! Gershman got injured! Replace her with Obama!"
+p hockey_rink[:humans][:home_team][:defense][:passive]
+hockey_rink[:humans][:home_team][:defense][:passive][1] = "Obama"
+p hockey_rink[:humans][:home_team][:defense][:passive]
+puts "How many saves does the starting goalie for the away team have?"
+p hockey_rink[:humans][:away_team][:goalies][:starter]
+puts "What brand of warrior sticks do we have?"
+p hockey_rink[:equipment][:sticks][:warrior]
