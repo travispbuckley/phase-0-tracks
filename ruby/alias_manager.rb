4420d85ed0144dd405d81678a74c9e8d824f1636
Travis Buckley <travispbuckley@lewisu.edu>
2016-09-04 22:32:40 -0500

    added commented portion for possible code usage

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -6,12 +6,38 @@
 # Make the methods separate, work on 1, get it right, and then implement it later
 def name_scrambler
 	puts "What is your first and last name?"
-	user_name = gets.chomp #sets user name
+	user_name = gets.chomp.downcase #sets user name, lowercases the strings
 	split_name = user_name.split(' ') #splits user name into 2 strings in an array
 	split_name.reverse! # puts the last name as first name, and saves in the array
 	split_name.join(' ')	
 end
 name_scrambler
+
+# def next_consonant(name)
+# 	name_array = name.downcase.chars
+# 	consonants = %w{b c d f g h j k l m n p q r s t v w x y z}
+# 	vowels = %w{a e i o u}
+# 	name_array.map! do |letter| 
+# 		# check for edge cases
+# 		if letter == "u"
+# 			letter = "a"
+#  		elsif vowels.include?(letter)
+#  			# if a letter is a vowel, reassign the index to that of the next vowel in the vowel array
+#  			new_vowel_index = vowels.index(letter) + 1
+#  			new_vowel = vowels[new_vowel_index]
+# 		elsif letter == "z"
+# 			letter = "b"
+# 		elsif consonants.include?(letter)
+# 			new_consonant_index = consonants.index(letter) + 1
+# 			new_consonant = consonants[new_consonant_index]
+# 		else
+# 			letter
+# 		end		
+# 	end
+# 	#Capitalize both first and last name
+# 	name_array.join('').split(" ").each{|i| i.capitalize!}.join(" ")
+# end
+# next_consonant("Travis Buckley")
 
 
 
