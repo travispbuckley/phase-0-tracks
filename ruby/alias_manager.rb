db0ad5fafff1414f74de160037e91787728f760a
Travis Buckley <travispbuckley@lewisu.edu>
2016-09-05 07:56:21 -0500

    added program to flip name and adv. each character

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -4,40 +4,34 @@
 # Change all vowels to next vowl (a=>e, e=>i i=>o o=>u u=>a)
 # Change all consonants to next consonent in alphabet (d=>f n=>p)
 # Make the methods separate, work on 1, get it right, and then implement it later
-def name_scrambler
-	puts "What is your first and last name?"
-	user_name = gets.chomp.downcase #sets user name, lowercases the strings
-	split_name = user_name.split(' ') #splits user name into 2 strings in an array
-	split_name.reverse! # puts the last name as first name, and saves in the array
-	split_name.join(' ')	
-end
-name_scrambler
 
-# def next_consonant(name)
-# 	name_array = name.downcase.chars
-# 	consonants = %w{b c d f g h j k l m n p q r s t v w x y z}
-# 	vowels = %w{a e i o u}
-# 	name_array.map! do |letter| 
-# 		# check for edge cases
-# 		if letter == "u"
-# 			letter = "a"
-#  		elsif vowels.include?(letter)
-#  			# if a letter is a vowel, reassign the index to that of the next vowel in the vowel array
-#  			new_vowel_index = vowels.index(letter) + 1
-#  			new_vowel = vowels[new_vowel_index]
-# 		elsif letter == "z"
-# 			letter = "b"
-# 		elsif consonants.include?(letter)
-# 			new_consonant_index = consonants.index(letter) + 1
-# 			new_consonant = consonants[new_consonant_index]
-# 		else
-# 			letter
-# 		end		
-# 	end
-# 	#Capitalize both first and last name
-# 	name_array.join('').split(" ").each{|i| i.capitalize!}.join(" ")
-# end
-# next_consonant("Travis Buckley")
+
+def name_scrambler(name)
+	name_to_array = name.downcase.chars
+	# This takes the string version of the name and makes it into an array, with each letter separated. 
+	consonants = %w{b c d f g h j k l m n p q r s t v w x y z}
+	vowels = %w{a e i o u}
+	# Setting the new strings we want our conditions to match
+	name_to_array.map! do |letter| 
+		if letter == "u"
+			letter = "a"
+ 		elsif vowels.include?(letter)
+ 			# if the letter returns a true value(is a vowel), re-assign a new index to that letter (advanced by 1)
+ 			new_vowel_index = vowels.index(letter) + 1
+ 			new_vowel = vowels[new_vowel_index]
+		elsif letter == "z"
+			letter = "b"
+		elsif consonants.include?(letter)
+			new_consonant_index = consonants.index(letter) + 1
+			new_consonant = consonants[new_consonant_index]
+		else
+			letter # Accounts for spaces
+		end		
+	end
+	#re capitalize by joining the indiv. letters, splitting into first and last, capitalizing, and rejoining
+	name_to_array.join('').split(" ").each{|i| i.capitalize!}.join(" ")
+end
+puts name_scrambler("Travis Buckley")
 
 
 
