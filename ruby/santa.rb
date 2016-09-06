b34c61a5aeec1efe8ce1e5bca99644027870bfa4
Travis Buckley <travispbuckley@lewisu.edu>
2016-09-06 13:03:50 -0500

    added info passsed in on initialization and default info

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,6 +1,10 @@
 class Santa
-	def initialize
-		puts "Initializing Santa instance ..."
+	def initialize(gender, ethnicity)
+		@gender = gender
+		@ethnicity = ethnicity
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer",
+		 "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		@age = 0 
 	end
 
 	def speak
@@ -10,7 +14,16 @@ class Santa
 	def eat_milk_and_cookies(cookie_type)
 		puts "That was a good #{cookie_type} cookie!"
 	end 
+	def santa_info
+		puts "This santa is a #{@gender}"
+		puts "This santa's ethnicity is #{@ethnicity}"
+		puts "This santa's age is #{@age}"
+	end 
 end 
-santa_one = Santa.new
-santa_one.speak
-santa_one.eat_milk_and_cookies("oatmeal raisin")
+# santa_one = Santa.new
+# santa_one.speak
+# santa_one.eat_milk_and_cookies("oatmeal raisin")
+santa = Santa.new("male", "caucasian")
+santa.speak
+santa.eat_milk_and_cookies("chocolate chip")
+santa.santa_info
