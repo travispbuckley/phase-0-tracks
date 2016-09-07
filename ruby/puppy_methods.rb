af0c57bf96042b4fc6ec9077c516f37a02badee9
Travis Buckley <travispbuckley@lewisu.edu>
2016-09-07 12:27:14 -0500

    added new methods and updated driver code

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,9 +1,29 @@
+# Worked with Shivani on this assignment
+
 class Puppy
 
   def fetch(toy)
     puts "I brought back the #{toy}!"
     toy
   end
+  def speak(n) 
+  	n.times {puts "woof"}
+  end
+  def roll_over
+  	puts "rolled over"
+  end 
+  def dog_years(n)
+  	dog_age = p n / 7
+  	puts "You are #{dog_age} years old!"
+  end 
+  def walk(distance)
+  	puts "you walked #{distance} miles!"
+  end
 
 end
-
+puppy = Puppy.new
+puppy.fetch("ball")
+puppy.speak(3)
+puppy.roll_over
+puppy.dog_years(70)
+puppy.walk(3)
