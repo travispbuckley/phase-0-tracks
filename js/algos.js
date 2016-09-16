bdf004b17d2f1334398d891543a131a6ba529287
Travis Buckley <travispbuckley@lewisu.edu>
2016-09-16 09:46:13 -0500

    pseudocoded and started the function

---
+++ js/algos.js
@@ -0,0 +1,10 @@
+// A function that takes in an array of strings
+// returns the longest word/phrase from the array
+	// Have to obtain length of each string
+	// Print the string with the largest number of length
+
+var arrayOfStrings = ['hello', 'hi', 'rip harambe'];
+// Established an array with varying lengths of each
+// now need a function to loop through the array, and find
+// the longest one
+function PhraseFinder()
