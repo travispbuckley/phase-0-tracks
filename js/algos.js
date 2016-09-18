f0ca8048f878b13cb682f76c653c74b07cf17581
Travis Buckley <travispbuckley@lewisu.edu>
2016-09-18 13:52:43 -0500

    added function to return equal key pairs

--- js/algos.js
+++ js/algos.js
@@ -1,7 +1,11 @@
-// A function that takes in an array of strings
-// returns the longest word/phrase from the array
-	// Have to obtain length of each string
-	// Print the string with the largest number of length
+/* A function that takes in an array of strings
+ returns the longest word/phrase from the array
+	 Have to obtain length of each string
+	 Print the string with the largest number of length
+
+	 Established an array with varying lengths of each
+ now need a function to loop through the array, and find
+ the longest one */
 function largestString(array)
 {
 	var arrayStrings = array;
@@ -16,10 +20,29 @@ function largestString(array)
  	return string;
 } 
 	
-// Established an array with varying lengths of each
-// now need a function to loop through the array, and find
-// the longest one
+function keyMatcher(object1, object2)
+{
+	if (object1.age === object2.age){
+		return true;
+	}
+	else {
+		return false;
+	}
+}
 
+var object1 = {
+	name: 'Travis', 
+	age: 24 
+};
+
+var object2 = {
+	name: 'Matt',
+	age: 24
+};
 
 var array = ['hello', 'hi', 'rip harambe'];
 console.log(largestString(array));
+
+console.log(keyMatcher(object1, object2));
+
+
