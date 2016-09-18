e219628102313e7f6d4b2df9217498b5fb999f34
Travis Buckley <travispbuckley@lewisu.edu>
2016-09-18 13:32:13 -0500

    wrote function for returning the longest string

--- js/algos.js
+++ js/algos.js
@@ -2,9 +2,24 @@
 // returns the longest word/phrase from the array
 	// Have to obtain length of each string
 	// Print the string with the largest number of length
-
-var arrayOfStrings = ['hello', 'hi', 'rip harambe'];
+function largestString(array)
+{
+	var arrayStrings = array;
+	string= '';
+	for (var i = 0; i<array.length; i++)
+	{
+		if (arrayStrings[i].length>string.length)
+		{
+			string=arrayStrings[i];
+		}
+	}
+ 	return string;
+} 
+	
 // Established an array with varying lengths of each
 // now need a function to loop through the array, and find
 // the longest one
-function PhraseFinder()
+
+
+var array = ['hello', 'hi', 'rip harambe'];
+console.log(largestString(array));
