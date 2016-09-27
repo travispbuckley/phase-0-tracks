30f3ede419e14be47182cd1ec9055b19afe52ba5
Travis Buckley <travispbuckley@lewisu.edu>
2016-09-27 12:03:25 -0500

    added function to make image smaller on hover

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -8,3 +8,14 @@ function switchPicture() {
 var photo = document.getElementById("Photo1");
 photo.addEventListener("click", switchPicture);
 
+var image = document.getElementById("Photo1"),
+    imageWidth = image.width,
+    imageHeight = image.height;
+image.onmouseover = function() {
+  image.width = .1 * imageWidth;
+  image.height = .1 * imageHeight;
+}
+image.onmouseout = function() {
+  image.width = imageWidth;
+  image.height = imageHeight;
+}
