b519066c79c4188a03f927599e7cfd64abcc89d7
Travis Buckley <travispbuckley@lewisu.edu>
2016-09-29 11:17:23 -0500

    created cars tab with updatable table

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -4,6 +4,18 @@ require 'sqlite3'
 
 set :public_folder, File.dirname(__FILE__) + '/static'
 
+car_database = SQLite3::Database.new("cars.db")
+car_database.results_as_hash = true
+
+create_table_cmd = <<-SQL
+  CREATE TABLE IF NOT EXISTS cars(
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(255)
+  )
+SQL
+
+car_database.execute(create_table_cmd)
+
 db = SQLite3::Database.new("students.db")
 db.results_as_hash = true
 
@@ -23,5 +35,20 @@ post '/students' do
   db.execute("INSERT INTO students (name, campus, age) VALUES (?,?,?)", [params['name'], params['campus'], params['age'].to_i])
   redirect '/'
 end
+
+get '/list_cars' do 
+	@cars=car_database.execute("select * from cars")
+  	erb :list_cars
+end
+
+get '/cars/new' do
+	erb :cars
+end
+
+post '/add_cars' do
+  car_database.execute("insert into cars (name) values(?)",[params['cars']])
+  redirect '/list_cars'
+end
+
 
 # add static resources
